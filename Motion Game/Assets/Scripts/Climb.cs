6beae579efa50886ad6df6762e8b27fa8749ce2c
StarManEXE <114677798+StarManEXE@users.noreply.github.com>
2023-03-10 11:41:49 -0500

    Wall Climb Added

---
+++ Motion Game/Assets/Scripts/Climb.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+
+public class Climb : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Rigidbody rb;
+    public PlayerMovement pm;
+    public LayerMask whatIsWall;
+
+    [Header("Climbing")]
+    public float climbSpeed;
+    public float maxClimbTime;
+    private float climbTimer;
+
+    private bool climbing;
+
+    [Header("Detection")]
+    public float detectionLength;
+    public float sphereCastRadius;
+    public float maxWallLookAngle;
+    private float wallLookAngle;
+
+    private RaycastHit frontWallHit;
+    private bool wallFront;
+
+    private void Update()
+    {
+        WallCheck();
+        StateMachine();
+
+        if (climbing) ClimbingMovement();
+    }
+
+    private void StateMachine()
+    {
+        // State 1 - Climbing
+        if (wallFront && Input.GetKey(KeyCode.W) && wallLookAngle < maxWallLookAngle)
+        {
+            if (!climbing && climbTimer > 0) StartClimbing();
+
+            // timer
+            if (climbTimer > 0) climbTimer -= Time.deltaTime;
+            if (climbTimer < 0) StopClimbing();
+        }
+
+        // State 3 - None
+        else
+        {
+            if (climbing) StopClimbing();
+        }
+    }
+
+    private void WallCheck()
+    {
+        wallFront = Physics.SphereCast(transform.position, sphereCastRadius, orientation.forward, out frontWallHit, detectionLength, whatIsWall);
+        wallLookAngle = Vector3.Angle(orientation.forward, -frontWallHit.normal);
+
+        if (pm.grounded)
+        {
+            climbTimer = maxClimbTime;
+        }
+    }
+
+    private void StartClimbing()
+    {
+        climbing = true;
+    }
+
+    private void ClimbingMovement()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, climbSpeed, rb.velocity.z);
+    }
+
+    private void StopClimbing()
+    {
+        climbing = false;
+    }
+}
